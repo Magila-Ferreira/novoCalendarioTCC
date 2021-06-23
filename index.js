9a04f49579f4d119f2c2868b9603fe3223061dcd
Mágila-Ferreira <magilamorganasf@hotmail.com>
2021-06-23 14:01:31 -0300

    Configurando o ejs + body-parser

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 // ******************************************************************
-// ouvir uma porta
+// Importar o express + body-parser
+const express = require('express')
+const bodyParser = require('body-parser')
 
+// Inicializar o express
+const app = express() 
+
+app.use('view engine', 'ejs')
+app.use(express.static('public'))
+
+// rota
+app.get('/', (req, res) => {
+    res.send('Bem vindo!!!')
+})  
+
+// ouvir uma porta
+app.listen(3000)
 
 // **************************** Configurando o GitHub
 
