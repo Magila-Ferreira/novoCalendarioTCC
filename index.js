aa3436c6d6d8ad29cd0d464961ec2acd0c08c061
Mágila-Ferreira <magilamorganasf@hotmail.com>
2021-06-23 23:55:35 -0300

    Criando rotas e configurando o JS

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 // Importar o express + body-parser
 const express = require('express')
 const bodyParser = require('body-parser')
+const { query } = require('express')
 
 // Inicializar o express
-const app = express() 
+const app = express()
 
 // Configurar o ejs e a pasta estática
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 // Configurar o bodyParser
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // rota
@@ -19,8 +20,24 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/adm', (req, res) => {
-    res.render('adm')
+app.get('/admin', (req, res) => {
+    res.render('admin')
+})
+
+app.post('/validarLogin', (req, res) => {
+    res.redirect('evento')
+})
+
+app.get('/evento', (req, res) => {
+    res.render('evento')
+})
+
+app.post('/salvarEvento', (req, res) => {
+    res.redirect('evento')
+})
+
+app.get('/calendario', (req, res) => {
+    res.render('calendario')
 })
 
 // ouvir uma porta
