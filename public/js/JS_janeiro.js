bb1e722c1cc13e95cc2049c49cef5d92bc4ccb0a
Mágila-Ferreira <magilamorganasf@hotmail.com>
2021-07-30 14:02:58 -0300

    Atualizando os calendários do select

--- public/js/JS_janeiro.js
+++ public/js/JS_janeiro.js
@@ -1,46 +1,45 @@
 // Variável que armazena todo o calendário
 var calendario = document.querySelector("#evento__calendario");
 
+// Variável que armazena o select
 var meses = document.getElementById("selectMes");
 
+// Função que chama o calendário correspondente ao mês do select
 function selectMes(valor) {
 
-    // Limpando o calendário
+    // Limpa o calendário antes do próximo select
     calendario.innerHTML = "";
 
+    // Condicional para chamar o mês de Janeiro
     if (valor === '1') {
 
-        // Chamando a função montaCalendario janeiro
+        // Chama a função populando o calendário
         populandoJaneiro(calendario);
-        
 
     } else if (valor === '2') {
 
-        // Chama a função montaCalendario fevereiro
-        montaCalendarioCinco(calendario);
+        populandoFevereiro(calendario);
 
     } else if (valor === '3') {
 
-        // Chama a função montaCalendario março
-        montaCalendarioCinco(calendario);
+        populandoMarco(calendario);
 
     } else if (valor === '4') {
 
-        // Chama a função montaCalendario abril
-        montaCalendarioCinco(calendario);
+        populandoAbril(calendario);
 
     } else if (valor === '5') {
 
-        // Chama a função montaCalendario maio
-        montaCalendarioSeis(calendario);
+        populandoMaio(calendario);
 
+        // Exceção para chamar o mês de Junho
     } else {
 
-        // Chama a função montaCalendario junho
-        montaCalendarioCinco(calendario);
+        populandoJunho(calendario);
     }
 
-    console.log("O mês selecionado foi: " + valor);
+    // Imprime o valor do mês selecionado na select
+    //console.log("O mês selecionado foi: " + valor);
 }
 
 // Funções:
@@ -49,6 +48,8 @@ function selectMes(valor) {
 
 // Cinco Semanas:
 function montaCalendarioCinco(cinco) {
+
+    // Laço que percorrerá as 5 linhas do calendário 
     for (var n = 0; n < 5; n++) {
 
         // Variável que criará as tr  
@@ -57,7 +58,7 @@ function montaCalendarioCinco(cinco) {
         // Relaciona cada tr como filha do calendário
         calendario.appendChild(semanaTr);
 
-        // Variável que armazena uma tr
+        // Laço que percorrerá as 7 colunas do calendário 
         for (var i = 0; i < 7; i++) {
 
             // Variável que cria cada td
@@ -67,29 +68,23 @@ function montaCalendarioCinco(cinco) {
             semanaTr.appendChild(diaTd);
         }
     }
+
+    // retorno da função montaCalendárioCinco
     return cinco;
 };
 
 // Seis Semanas:
 function montaCalendarioSeis(seis) {
+
     for (var n = 0; n < 6; n++) {
-
-        // Variável que criará as tr  
         var semanaTr = document.createElement("tr");
-
-        // Relaciona cada tr como filha do calendário
         calendario.appendChild(semanaTr);
 
         for (var i = 0; i < 7; i++) {
-
-            // Variável que cria cada td
             var diaTd = document.createElement("td");
-
-            // Relaciona cada td como filha da tr
             semanaTr.appendChild(diaTd);
         }
     }
-
     return seis;
 };
 
@@ -98,26 +93,120 @@ function montaCalendarioSeis(seis) {
 // JANEIRO:
 function populandoJaneiro(janeiro) {
 
+    // Variável que armazena o calendário de janeiro
     var janeiro = montaCalendarioSeis(calendario);
 
+    // Variável que armazena todas as td do calendário de janeiro
     var tdDias = janeiro.querySelectorAll("td");
 
+    // Variável que armazena o primeiro dia do mês
+    var dia = 1;
+
+    // Laço que percorrerá todas as td do mês
+    for (var i = 0; i < tdDias.length; i++) {
+
+        // Variável flexível - conforme o laço percorre as td, a variável tdDia corresponde a td do i que o laço está percorrendo, até que todo o array tdDias seja percorrido dentro da definição máxima do i. 
+        var tdDia = tdDias[i];
+
+        // Condicional que determina o início e fim do mês dentro das td 
+        if (i > 4 && i < 36) {
+
+            // Popula a tdDia do laço com o valor do dia
+            tdDia.textContent = dia;
+
+            // Atualiza o valor do dia a cada volta do laço
+            dia++;
+        }
+    }
+
+    // retorna o mês completo 
+    return janeiro;
+}
+
+// FEVEREIRO:
+function populandoFevereiro(fevereiro) {
+    var fevereiro = montaCalendarioCinco(calendario);
+    var tdDias = fevereiro.querySelectorAll("td");
+    var dia = 1;
+
+    for (var i = 0; i < tdDias.length; i++) {
+        var tdDia = tdDias[i];
+ 
+        if (i > 0 && i < 29) {
+            tdDia.textContent = dia;
+            dia++;
+        }
+    }
+    return fevereiro;
+}
+
+// MARÇO:
+function populandoMarco(marco) {
+    var marco = montaCalendarioCinco(calendario);
+    var tdDias = marco.querySelectorAll("td");
     var dia = 1;
 
     for (var i = 0; i < tdDias.length; i++) {
         var tdDia = tdDias[i];
 
-        if (i > 4 && i < 36) {
+        if (i > 0 && i < 32) {
             tdDia.textContent = dia;
             dia++;
         }
     }
-
-    return janeiro;
+    return marco;
 }
 
+// ABRIL
+function populandoAbril(abril) {
+    var abril = montaCalendarioCinco(calendario);
+    var tdDias = abril.querySelectorAll("td");
+    var dia = 1;
 
+    for (var i = 0; i < tdDias.length; i++) {
+        var tdDia = tdDias[i];
 
+        if (i > 3 && i < 34) {
+            tdDia.textContent = dia;
+            dia++;
+        }
+    }
+    return abril;
+}
+
+// MAIO:
+function populandoMaio(maio) {
+    var maio = montaCalendarioSeis(calendario);
+    var tdDias = maio.querySelectorAll("td");
+    var dia = 1;
+
+    for (var i = 0; i < tdDias.length; i++) {
+        var tdDia = tdDias[i];
+
+        if (i > 5 && i < 37) {
+            tdDia.textContent = dia;
+            dia++;
+        }
+    }
+    return maio;
+}
+
+// JUNHO:
+function populandoJunho(junho) {
+    var junho = montaCalendarioCinco(calendario);
+    var tdDias = junho.querySelectorAll("td");
+    var dia = 1;
+
+    for (var i = 0; i < tdDias.length; i++) {
+        var tdDia = tdDias[i];
+
+        if (i > 1 && i < 32) {
+            tdDia.textContent = dia;
+            dia++;
+        }
+    } 
+    return junho;
+}
 
 /*console.log("O indice é: " + meses.selectedIndex);
 console.log("O texto é: " + meses.options[meses.selectedIndex].text);
